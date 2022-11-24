8aea6aafc76681f8e5f251e5d077d507ac1fe0f6
AUnicornWithNoLife <55228370+AUnicornWithNoLife@users.noreply.github.com>
2022-11-24 20:47:48 +0000

    credits.txt

--- Blackjack2022/FileLib.cs
+++ Blackjack2022/FileLib.cs
@@ -10,6 +10,7 @@ namespace Blackjack2022
     {
         public const string SETTINGS_FILE_LOCATION = "/SETTINGS/settings.json";
         public const string RULES_FILE_LOCATION = "/Ass/Rules.txt";
+        public const string CREDITS_FILE_LOCATION = "/Ass/Credits.txt";
 
         public static string GetFullAddress(string file)
         {
