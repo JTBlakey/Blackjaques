eb891177a345d889f6c740f4a021e12b9b74d7ab
AUnicornWithNoLife <55228370+AUnicornWithNoLife@users.noreply.github.com>
2022-12-27 22:50:30 +0000

    class

---
+++ Blackjack2022/Data/Stats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Blackjack2022
+{
+	public class Stats
+	{
+		public long gamesPlayed { get; set; }
+
+		public long moneyMade { get; set; }
+		public long moneyLost { get; set; }
+
+		public long totalCardsHit { get; set; }
+		public float averageCardsHit { get { return totalCardsHit / gamesPlayed; } }
+
+		public long totalWin { get; set; }
+		public long totalLoss { get; set; }
+		public float winChance { get { return totalWin / gamesPlayed; } }
+
+		public Stats()
+		{
+			this.gamesPlayed = 0;
+
+			this.moneyMade = 0;
+			this.moneyLost = 0;
+
+			this.totalCardsHit = 0;
+
+			this.totalWin = 0;
+			this.totalLoss = 0;
+		}
+	}
+}
+
